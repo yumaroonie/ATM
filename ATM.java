ddf884c5678da73fd026165cac01bd8768ae31f9
Chris Headley <chrisheadley20@gmail.com>
2023-09-04 21:46:51 -0700

    Created hash map, constructor

--- ATM.java
+++ ATM.java
@@ -4,5 +4,10 @@ import java.io.PrintWriter;
 import java.util.HashMap;
 
 public class ATM{
-    
+    private HashMap <String, Double> accountMap;
+
+    public ATM ()
+    {
+        accountMap = new HashMap <String, Double> ();
+    }
 }
